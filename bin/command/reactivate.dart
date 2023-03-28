bddf3152693a82e0614ead9434b085adb1871046
Rexios <rexios@rexios.dev>
2023-03-28 19:48:46 -0400

    ...

--- bin/command/reactivate.dart
+++ bin/command/reactivate.dart
@@ -1,4 +1,5 @@
 import 'package:args/command_runner.dart';
+import 'package:collection/collection.dart';
 import 'package:rex/util.dart';
 
 class ReactivateCommand extends Command {
@@ -6,14 +7,40 @@ class ReactivateCommand extends Command {
   final name = 'reactivate';
 
   @override
-  final description = 'Reactivate this tool from source';
+  final description =
+      'Reactivate a pub package. If no package is specified, rex will be reactivated from git. Specify "." to reactivate the current directory.';
+
+  @override
+  final invocation = 'rex reactivate [package]';
 
   @override
   Future<void> run() async {
-    print('Reactivating...');
-    await runProcess(
-      'dart',
-      ['pub', 'global', 'activate', '--source', 'path', '$home/repos/rex'],
-    );
+    final package = argResults?.rest.firstOrNull;
+
+    if (package == null) {
+      print('Reactivating rex...');
+      await runProcess('dart', [
+        'pub',
+        'global',
+        'activate',
+        '--source',
+        'git',
+        'https://github.com/Rexios80/rex',
+      ]);
+    } else if (package == '.') {
+      print('Reactivating current directory...');
+      await runProcess('dart', [
+        'pub',
+        'global',
+        'activate',
+        '--source',
+        'path',
+        '.',
+      ]);
+    } else {
+      print('Reactivating $package...');
+      await runProcess('dart', ['pub', 'global', 'deactivate', package]);
+      await runProcess('dart', ['pub', 'global', 'activate', package]);
+    }
   }
 }
