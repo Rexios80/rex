18f846f3f6174bb99136c034db18e3c7fbf0cd1d
Rexios <rexios@rexios.dev>
2024-05-29 15:10:17 -0400

    Add script flutterRunWasm

--- lib/scripts.dart
+++ lib/scripts.dart
@@ -3,6 +3,7 @@ const scriptMap = {
   'reset-xcode': resetXcode,
   'git-init': gitInit,
   'gradle-sync': gradleSync,
+  'flutter-run-wasm': flutterRunWasm,
 };
 
 /// https://gist.github.com/maciekish/66b6deaa7bc979d0a16c50784e16d697
@@ -25,3 +26,8 @@ git commit -m "Initial commit"''';
 /// Gradle sync
 const gradleSync = r'''
 ./gradlew prepareKotlinBuildScriptModel''';
+
+/// Helper to emulate `flutter run --wasm` until that command hits Flutter stable
+const flutterRunWasm = r'''
+flutter build web --wasm --no-strip-wasm
+dhttpd "--headers=Cross-Origin-Embedder-Policy=credentialless;Cross-Origin-Opener-Policy=same-origin" --path build/web''';
